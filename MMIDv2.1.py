0014a1e7902d3bea2de0d93fb26f90e79a13332f
davidmasp <david.mas.p@gmail.com>
2016-02-23 23:54:46 +0100

    this is a final iteration main script

--- MMIDv2.1.py
+++ MMIDv2.1.py
@@ -1,13 +1,14 @@
 #!/usr/local/bin/python3
 
 ########################
-#    M.M.D.I. v2       #
+#    M.M.D.I. v2.1     #
 ########################
 
 ###############################################################################
-#              version2: This version is including the production             #
+#              version2.1: This version is including the production           #
 # of the scores for the testing background and foreground sets to             #
-# generate the ROC and PPV curves.                                            #
+# generate the ROC and PPV curves.  the 2.1 uses iterations to get            #
+# multiple values.                                                            #
 ###############################################################################
 
 
@@ -48,63 +49,63 @@ parser.add_argument('-o','--MM_output', dest='outfile', action='store',
                     help='The root output file where the MM and score values is going to be written.')
 parser.add_argument('-p','--pseudocounts', dest='pseudocounts',
                     action='store_true',
-                    default=False,
+                    default=True,
                     help="Add pseudocounts in the generated MM"
                     )
-parser.add_argument('-k','--order',dest='order',action='store',type=int,
-                    default=3,
-                    help='It sets the order of the Markov Model used.')
-
-
-parser.add_argument('-l','--windows',dest='wsize',action='store',type=int,
-                    default=None,
-                    help='It sets the windows size to analize the testing set.')
-
 
 args = parser.parse_args()
 
+k_list=[2,3,4,5,6,7,8]
+l_list=[15,20,25,30,35,40,45]
+for k in k_list:
+    for l in l_list:
+        if k<l:
+            sys.stderr.write("The log begins, k-order = {} and windows length = {}".format(k,l))
+            training_filename='training_'+args.infile
+            testing_filename='testing_'+args.infile
+            sys.stderr.write("Starting the Log:\nThe input data is from {}\n".format(args.infile))
+            sys.stderr.write("Arguments:\nMM Order:{}\nWindows Length:{}\n".format(k,l))
+            if args.pseudocounts:
+                sys.stderr.write("This run is using pseudocounts.\n\n\n")
+            sys.stderr.write("Starting the training and testing separation...\n\n")
+            m.training_testing_sets_separation(args.infile,3,training_filename,testing_filename)
+
+            ######## 2 +/- ###########
+
+
+            ######## 3 kmer dict #####
+
+
+            if (args.pseudocounts):
+                sys.stderr.write("Starting the training background and foreground separation...\n\n")
+                back_dict=m.build_hash_pseudocount([x for x in m.background_separation(training_filename)],k)
+                sys.stderr.write("The MM for the bg is generated...\n\n")
+                sign_dict=m.build_hash_pseudocount([x for x in m.signal_separation(training_filename)],k)
+                sys.stderr.write("The MM for the bg is generated...\n\n")
+            else:
+                back_dict=m.build_hash([x for x in m.background_separation(training_filename)],k)
+                sign_dict=m.build_hash([x for x in m.signal_separation(training_filename)],k)
+
+            ######### 4 print ########
+            output_filename=args.outfile+"_k_"+str(k)+"_l_"+str(l)+'.MM'
+            sys.stderr.write("Printing the model in {}...\n\n".format(output_filename))
+            m.print_hash(sign_dict,back_dict,output_filename)
+
+            ######## 5 windows #######
+            sys.stderr.write("Starting the testing background and foreground separation...\n\n")
+
+            sys.stderr.write("Starting the computation of the Scores...\n\n")
+            output_filename_fg=args.outfile+"_k_"+str(k)+"_l_"+str(l)+'_fg.th'
+            output_filename_bg=args.outfile+"_k_"+str(k)+"_l_"+str(l)+'_bg.th'
+            out_fg=open(output_filename_fg, "w")
+            out_bg=open(output_filename_bg, "w")
+            sys.stderr.write("Printing the background scores at {}...\n\n".format(output_filename_bg))
+            for i in m.windows_score([x for x in m.background_separation(testing_filename)],k,l, back_dict,sign_dict):
+                out_bg.write("{}\n".format(i))
+            sys.stderr.write("Printing the foreground scores at {}...\n\n".format(output_filename_fg))
+            for i in m.windows_score([x for x in m.signal_separation(testing_filename)],k,l, back_dict,sign_dict):
+                out_fg.write("{}\n".format(i))
+        else:
+            pass
+
 ######## 1 TT SETS ######
-training_filename='training_'+args.infile
-testing_filename='testing_'+args.infile
-sys.stderr.write("Starting the Log:\nThe input data is from {}\n".format(args.infile))
-sys.stderr.write("Arguments:\nMM Order:{}\nWindows Length:{}\n".format(args.order,args.wsize))
-if args.pseudocounts:
-    sys.stderr.write("This run is using pseudocounts.\n\n\n")
-sys.stderr.write("Starting the training and testing separation...\n\n")
-m.training_testing_sets_separation(args.infile,3,training_filename,testing_filename)
-
-######## 2 +/- ###########
-
-
-######## 3 kmer dict #####
-
-
-if (args.pseudocounts):
-    sys.stderr.write("Starting the training background and foreground separation...\n\n")
-    back_dict=m.build_hash_pseudocount([x for x in m.background_separation(training_filename)],args.order)
-    sys.stderr.write("The MM for the bg is generated...\n\n")
-    sign_dict=m.build_hash_pseudocount([x for x in m.signal_separation(training_filename)],args.order)
-    sys.stderr.write("The MM for the bg is generated...\n\n")
-else:
-    back_dict=m.build_hash([x for x in m.background_separation(training_filename)],args.order)
-    sign_dict=m.build_hash([x for x in m.signal_separation(training_filename)],args.order)
-
-######### 4 print ########
-output_filename=args.outfile+'.MM'
-sys.stderr.write("Printing the model in {}...\n\n".format(output_filename))
-m.print_hash(sign_dict,back_dict,output_filename)
-
-######## 5 windows #######
-sys.stderr.write("Starting the testing background and foreground separation...\n\n")
-
-sys.stderr.write("Starting the computation of the Scores...\n\n")
-output_filename_fg=args.outfile+'_fg.th'
-output_filename_bg=args.outfile+'_bg.th'
-out_fg=open(output_filename_fg, "w")
-out_bg=open(output_filename_bg, "w")
-sys.stderr.write("Printing the background scores at {}...\n\n".format(output_filename_bg))
-for i in m.windows_score([x for x in m.background_separation(testing_filename)],args.order,args.wsize, back_dict,sign_dict):
-    out_bg.write("{}\n".format(i))
-sys.stderr.write("Printing the foreground scores at {}...\n\n".format(output_filename_fg))
-for i in m.windows_score([x for x in m.signal_separation(testing_filename)],args.order,args.wsize, back_dict,sign_dict):
-    out_fg.write("{}\n".format(i))
